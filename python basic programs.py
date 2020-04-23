932d15bb95e0887845277ddb9d65735bcabc4574
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-24 00:56:08 +0530

    Add files via upload

---
+++ python basic programs.py
@@ -0,0 +1,130 @@
+print("Hello World")
+-----------------------
+#This is a comment.
+print("Hello, World!")
+------------------------
+"""This is a
+multiline docstring."""
+print("Hello, World!")
+------------------------
+x = 5
+y = "John"
+print(x)
+print(y)
+--------------------------
+x = "awesome"
+print("Python is " + x)
+--------------------------
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+--------------------------
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+--------------------------
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+--------------------------
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+----------------------
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+-------------------------
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+------------------------
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+--------------------------
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+----------------------------
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+---------------------------
+a = "Hello, World!"
+print(a[1])
+--------------------------
+b = "Hello, World!"
+print(b[2:5])
+--------------------------
+a = " Hello, World! "
+print(a.strip())
+--------------------------
+a = "Hello, World!"
+print(len(a))
+---------------------------
+a = "Hello, World!"
+print(a.lower())
+---------------------------
+a = "Hello, World!"
+print(a.upper())
+--------------------------
+a = "Hello, World!"
+print(a.replace("H", "J"))
+---------------------------
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+--------------------------
+x = 5
+y = 3
+print(x + y)
+-------------------------
+x = 5
+y = 3
+print(x - y)
+------------------------
+x = 5
+y = 3
+print(x * y)
+-------------------------
+x = 12
+y = 3
+print(x / y)
+-------------------------
+x = 5
+y = 2
+print(x % y)
+-------------------------
+x = 5
+print(x)
+
+
+
