a6abfceda312a5aab04789103c0ff57378b9345d
ZumeITInc <63601118+ZumeITInc@users.noreply.github.com>
2020-04-24 01:10:55 +0530

    Add files via upload

---
+++ python basic programs.py
@@ -0,0 +1,156 @@
+print("Hello World")
+
+#This is a comment.
+print("Hello, World!")
+
+
+"""This is a
+multiline docstring."""
+print("Hello, World!")
+
+# Create a variable
+x = 5
+y = "John"
+print(x)
+print(y)
+
+# Output both text and a variable
+x = "awesome"
+print("Python is " + x)
+
+# Add a variable to  another variable
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+
+# Verify the type of an object
+x = 1
+y = 2.8
+z = 1j
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Create integers
+x = 1
+y = 35656222554887711
+z = -3255522
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Create floating point numbers
+x = 1.10
+y = 1.0
+z = -35.59
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Create specific numbers with an 'e' to indicate the power of 10
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Create complex numbers
+x = 3+5j
+y = 5j
+z = -5j
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Casting-Integers
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+# Casting-Floats
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+# Casting-Strings
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+# Get the charecter at position 1 of string
+a = "Hello, World!"
+print(a[1])
+
+# Substring. Get the characters of a position 2 to position 5 (not included)
+b = "Hello, World!"
+print(b[2:5])
+
+#REmove whitespace from the beginning or at the end of a string
+a = " Hello, World! "
+print(a.strip())
+
+# Return the length of a string
+a = "Hello, World!"
+print(len(a))
+
+# Convert a string into lowe case
+a = "Hello, World!"
+print(a.lower())
+
+# Covert a string into upper case
+a = "Hello, World!"
+print(a.upper())
+
+# Replace a string with anothore string
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+# Split a string into substring
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+
+# Addition Operator
+x = 5
+y = 3
+print(x + y)
+
+# Subtraction Operator
+x = 5
+y = 3
+print(x - y)
+
+# Multiplication Operator
+x = 5
+y = 3
+print(x * y)
+
+# Devision Operator
+x = 12
+y = 3
+print(x / y)
+
+# Modulus Operator
+x = 5
+y = 2
+print(x % y)
+
+# Assignment Operator
+x = 5
+print(x)
+
+
+
